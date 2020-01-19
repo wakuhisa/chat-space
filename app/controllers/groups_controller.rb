fc2e21e8b10c47c39d9f57b9b6d205b1495341c4
wakuhisa <1bpj2117@gmail.com>
2020-01-19 19:26:36 +0900

    add edit, update action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,9 +15,16 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
 
   private
