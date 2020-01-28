0f69fd9ae6ad2a57330b41b803a70b4d320b83ab
wakuhisa <1bpj2117@gmail.com>
2020-01-28 15:46:37 +0900

    add link Edit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @users = User.all
   end
 
   def create
